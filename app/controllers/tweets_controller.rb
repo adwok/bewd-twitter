b92ab8b03e6f177c4bae01c532c5dde23137c207
Adam Chan <adwok@Adams-MacBook-Air.local>
2023-05-30 01:06:36 +0800

    ccoommiitt

--- app/controllers/tweets_controller.rb
+++ app/controllers/tweets_controller.rb
@@ -1,2 +1,58 @@
 class TweetsController < ApplicationController
+  def create
+    @tweet = Tweet.new(tweet_params)
+    token = cookies.permanent.signed[:twitter_session_token]
+    session = Session.find_by(token: token)
+
+    if session
+      @user = session.user
+      if @tweet.save
+        render json: {
+          tweet: {
+            username: @user.username,
+            message: @tweet.message
+          }
+        }
+      end
+    end
+  end
+
+  def destroy
+    token = cookies.permanent.signed[:twitter_session_token]
+    session = Session.find_by(token: token)
+    @tweet = Tweet.find_by(id: params[:id])
+    if session
+      if @tweet and @tweet.destroy
+        render json: { success: true }
+      else
+        render json: { success: false }
+      end
+    else 
+      render json: { success: false }
+    end
+  end
+
+  def index
+    @tweets = Tweet.all.sort.reverse
+  end
+
+  def index_by_user
+    token = cookies.permanent.signed[:twitter_session_token]
+    session = Session.find_by(token: token)
+    if session
+      @user = session.user
+      @tweets = Tweets.find_by(username: @user.username).all
+    end
+  end
+    
+  private
+
+    def tweet_params
+      params.require(:tweet).permit(:message)
+    end
+
+  
+
+  
+
 end
